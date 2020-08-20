73e63891cd6d202a7d0807a484cb016d968338b4
SocialGuru80 <66398681+SocialGuru80@users.noreply.github.com>
2020-08-20 19:27:48 +0000

    cka-vip

--- ckadmin/resources/views/layouts/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- RIGHT SIDEBAR -->
-            <!-- ========================================================= -->
-             {{-- @include('common.right_sidebar') --}}
-            <!--scroll to top-->
-            <a href="#" class="scroll-to-top"><i class="fa fa-angle-double-up"></i></a>
